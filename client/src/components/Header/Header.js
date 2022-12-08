f57cf4a4eae3fda2b09a9091f766aa6d5689e435
fisal <fisaljabbarin@gmail.com>
2022-12-08 13:21:12 +0100

    hundred times

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className= 'header'>
+      <h1> Eddros </h1>
+      <p> Hallo student</p>
+      
+    </div>
+  )
+}
